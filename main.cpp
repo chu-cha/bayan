1d6f4a7cdbb73dedb5502a64bbdffba1efc1558d
tk <tkardashova@thor3dscanner.com>
2025-05-21 17:31:55 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+int main(int argc, char* argv[]) {
+
+    
+
+    return 0;
+}
